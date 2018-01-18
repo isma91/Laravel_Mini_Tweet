e7ff64ffcdb7591c5d1d2b99cec1c8aa6d5f4fcd
isma91 <ismaydogmus@gmail.com>
2018-01-18 20:42:05 +0100

    using usercontroller method to display view

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,33 @@
 |
 */
 
-Route::get('/', function () {
-    return view('login');
-});
+Route::get('/', 'UserController@displayLogin');
 
-Route::get('/login', function () {
-    return view('login');
-});
+/* testing to use redirect action but laravel can't find my method... */
+/*Route::get('/{page}', function ($page) {
+    if ($page == "/" || empty($page)) {
+        $page = "login";
+    }
+    $allowedPageGuest = ['login', 'register'];
+    foreach ($allowedPageGuest as  $value) {
+        if ($page === $value) {
+            return redirect()->action('UserController@display', ['page' => $value]);
+            break;
+        }
+    }
+    return abort(404);
+});*/
+
+Route::get('/login', 'UserController@displayLogin');
 
 Route::post('/login', 'UserController@login');
 
-Route::get('/register', function () {
-    return view('register');
-});
+Route::get('/register', 'UserController@displayRegister');
 
 Route::post('/register', 'UserController@register');
 
-Route::get('/forgotPass', function () {
-    return view('forgotPass');
-});
+Route::get('/forgotPass', 'UserController@displayForgotPass');
 
-Route::get('/home', 'PanelController@home');
+Route::post('/logout', 'UserController@logout');
+
+Route::get('/home', 'PanelController@displayHome');
