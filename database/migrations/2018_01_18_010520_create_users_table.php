6c8d163f3a3305d8313759a4c2c655cb7d1e43f0
isma91 <ismaydogmus@gmail.com>
2018-01-18 02:34:59 +0100

    creating the users table with artisan

---
+++ database/migrations/2018_01_18_010520_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('login')->unique();
+            $table->text('pass');
+            $table->string('email')->unique();
+            $table->text('token');
+            $table->text('avatar')->nullable();
+            $table->integer('active')->default(1);
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
