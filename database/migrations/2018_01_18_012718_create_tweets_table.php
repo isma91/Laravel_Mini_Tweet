b81b3cae708eeab286e4e8c2e84ce1bfb4e14b33
isma91 <ismaydogmus@gmail.com>
2018-01-18 02:35:12 +0100

    creating the tweets table with artisan

---
+++ database/migrations/2018_01_18_012718_create_tweets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->text('favorite');
+            $table->text('love');
+            $table->text('content');
+            $table->integer('active')->default(1);
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tweets');
+    }
+}
